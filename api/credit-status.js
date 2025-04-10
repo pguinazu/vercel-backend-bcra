daa9c545bd4bffb97dce1288fbfd3e389419b24f
Pablo Guiñazu <Pablo_Guinazu@intranet.itresources>
2025-04-10 00:14:16 -0300

    first commit

---
+++ api/credit-status.js
@@ -0,0 +1,22 @@
+// api/credit-status.js
+const axios = require('axios');
+
+if (process.env.NODE_ENV !== 'production') {
+  process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
+}
+
+module.exports = async (req, res) => {
+  const { cuil } = req.query;
+
+  if (!cuil) {
+    return res.status(400).json({ error: 'CUIL requerido en la query' });
+  }
+
+  try {
+    const response = await axios.get(`http://api.bcra.gob.ar/centraldedeudores/v1.0/Deudas/${cuil}`);
+    res.status(200).json(response.data);
+  } catch (error) {
+    console.error('Error al consultar BCRA:', error.message);
+    res.status(500).json({ error: 'No se pudo obtener datos del BCRA' });
+  }
+};
